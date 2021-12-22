7a559f940973e8baadeeb4eadb9680c120c489d7
Francisco García <francoffee57@gmail.com>
2021-12-22 17:26:42 -0600

    Agregué una función para ordenar los tareas dependiendo si estan completas o no

--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ import {CreateTodoButton} from './CreateTodoButton'
 import {TodoItem} from './TodoItem'
 
 let todos = [
-  {text: "Lavar el carro", complete: true},
-  {text: "Sacar al perro", complete: false},
+  {text: "Lavar el carro", complete: false},
+  {text: "Sacar al perro", complete: true},
   {text: "Limpiar el cuarto", complete: true},
   {text: "Tomar la pastilla", complete: false},
   {text: "Salir a correr", complete: true},
 ]
+const orderTodos = (todo) =>{
+  const todoComplete = todo.filter(item => item.complete)
+  const todoIncomplete = todo.filter(item => !item.complete)
+
+  const rta = []
+  rta.push(...todoIncomplete, ...todoComplete)
+  return rta
+}
+const todoList = orderTodos(todos)
 
 function App(props) {
   return (
@@ -24,7 +33,7 @@ function App(props) {
 
      <TodoList>
        <h3 className="todo-list__title"> <span><i className="fas fa-list-ul"></i></span> TodoList</h3>
-        {todos.map(todo => (
+        {todoList.map(todo => (
           <TodoItem key={todo.text} complete={todo.complete} text={todo.text}/>
         ))}
      </TodoList>
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,12 @@ import React from "react";
 import './styles/TodoSearch.css'
 
 function TodoSearch(props){
+
+    const [searchValue, setSearchValue] = React.useState('')
+
     const onSearchValueChange = (event) =>{
         console.log(event.target.value);
+        setSearchValue(event.target.value)
     }
     return(
         <div className="search__container">
@@ -11,11 +15,13 @@ function TodoSearch(props){
             className="search" 
             type="text" 
             placeholder="ToDo" 
+            value={searchValue}
             onChange={onSearchValueChange}
         ></input>
          <button className="search__button">
              <i className=" search__icon fas fa-search"></i>
         </button>
+        <p className="me-borras-pendejo">{searchValue}</p>
        </div>
     )
 }
